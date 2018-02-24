581a3f610b571e245179bf5e5d6d291e42a0ba01
Szymon Kolasa <szymonkolasa@hotmail.com>
2018-02-24 17:15:53 +0100

    Add project files.

---
+++ WebApi.Console/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace WebApi.CUI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var pokemons = new List<Pokemon>();
+
+            var url = new Uri(@"http://localhost:53580/");
+
+            var client = new HttpClient();
+            client.BaseAddress = url;
+            var result = await client.GetAsync(@"api/Pokemon");
+
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var content = await result.Content.ReadAsStringAsync();
+                pokemons = JsonConvert.DeserializeObject<List<Pokemon>>(content);
+
+                foreach (var pokemon in pokemons)
+                {
+                    Console.WriteLine($"{pokemon.Id} {pokemon.Name} {pokemon.Type}");
+                }
+            }
+
+            client.Dispose();
+
+            var upd = pokemons.First();
+
+            client = new HttpClient();
+            client.BaseAddress = url;
+
+            result = await client.PostAsync(@"api/Post/" + upd.Id, new StringContent(JsonConvert.SerializeObject(upd)));
+
+            if (result.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Pokemon updated!");
+            }
+
+            client.Dispose();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ WebApi.SPA/Controllers/PokemonController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.SPA.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Pokemon")]
+    public class PokemonController : Controller
+    {
+        private ICollection<Pokemon> _pokemons;
+
+        public PokemonController()
+        {
+            _pokemons = new List<Pokemon>(new Pokemon[]
+            {
+                new Pokemon { Id = 1, Name = "Jigglypuff", Type = "Fairy" },
+                new Pokemon { Id = 2, Name = "Pikachu", Type = "Electric" },
+                new Pokemon { Id = 3, Name = "Charizard", Type = "Fire" },
+                new Pokemon { Id = 4, Name = "Bulbasaur", Type = "Grass" }
+            });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get() => Ok(_pokemons);
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var item = _pokemons.FirstOrDefault(x => x.Id == id);
+
+            if (item is null)
+                return BadRequest("Pokemon not found");
+            else
+                return Ok(item);
+        }
+
+        [HttpPut("[action]")]
+        public IActionResult Put([FromBody]Pokemon model)
+        {
+            if (ModelState.IsValid)
+            {
+                _pokemons.Add(model);
+                return Created("/api/Pokemon/Put", model);
+            }
+
+            return BadRequest(ModelState.ValidationState);
+        }
+
+        [HttpPost("[action]/{id}")]
+        public IActionResult Post([FromRoute]int id, [FromBody]Pokemon model)
+        {
+            var pokemon = _pokemons.FirstOrDefault(x => x.Id == id);
+
+            if (pokemon is null)
+                return BadRequest("Wrong id");
+
+            if (ModelState.IsValid)
+            {
+                pokemon.Id = model.Id;
+                pokemon.Name = model.Name;
+                pokemon.Type = model.Type;
+
+                return Ok(pokemon);
+            }
+
+            return BadRequest(ModelState.ValidationState);
+        }
+
+        [HttpDelete("[action]/{id}")]
+        public IActionResult Delete([FromRoute]int id)
+        {
+            var pokemon = _pokemons.FirstOrDefault(x => x.Id == id);
+
+            if (pokemon is null)
+                return BadRequest("Pokemon not found");
+
+            _pokemons.Remove(pokemon);
+
+            return Ok("Removed");
+        }
+    }
+
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
